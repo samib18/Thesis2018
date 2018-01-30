99106f9d91f77805746b0cb8b78ee2b0999de2ca
samib18 <sami.bialozynski@gmail.com>
2018-01-30 15:59:44 -0500

    Updates Jan 30

--- app/src/main/java/edu/samibialozynski2washcoll/thesis2018/MainActivity.java
+++ app/src/main/java/edu/samibialozynski2washcoll/thesis2018/MainActivity.java
@@ -2,26 +2,36 @@ package edu.samibialozynski2washcoll.thesis2018;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.jsoup.nodes.Document;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView schedule;
+    ArrayList<String> classList;
     Button sami;
     Button elle;
     Button court;
     Button liza;
+    ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         sami = findViewById(R.id.samiButton);
         elle = findViewById(R.id.elleButton);
@@ -31,27 +41,9 @@ public class MainActivity extends AppCompatActivity {
         sami.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*InputStream stream = getResources().openRawResource(R.raw.samispringschedule);
-                InputStreamReader inputreader = new InputStreamReader(stream);
-                BufferedReader buffreader = new BufferedReader(inputreader);
-                String line;
-                StringBuilder text = new StringBuilder();
 
-                try {
-                    while((line = buffreader.readLine()) != null) {
-                        text.append(line);
-                    }
-                }catch (IOException e){
-                    e.printStackTrace();
-                }*/
-
-                TextView schedule = findViewById(R.id.samiSchedule);
+                schedule = findViewById(R.id.samiSchedule);
                 schedule.setText(getSchedule());
-
-                //Intent myIntent = new Intent(MainActivity.this,
-                        //Schedule.class);
-                //myIntent.putExtra("Schedule", text.toString());
-                //startActivity(myIntent);
 
             }
         });
@@ -59,27 +51,27 @@ public class MainActivity extends AppCompatActivity {
         elle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView schedule = findViewById(R.id.samiSchedule);
-                schedule.setText(getSchedule());
+                schedule = findViewById(R.id.samiSchedule);
+                //schedule.setAdapter(getSchedule());
             }
         });
 
         court.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView schedule = findViewById(R.id.samiSchedule);
-                schedule.setText(getSchedule());
+                schedule = findViewById(R.id.samiSchedule);
+                //schedule.setAdapter(getSchedule());
             }
         });
 
         liza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView schedule = findViewById(R.id.samiSchedule);
-                schedule.setText(getSchedule());
+
+                schedule = findViewById(R.id.samiSchedule);
+                //schedule.setAdapter(getSchedule());
             }
         });
-
     }
     public String getSchedule(){
         InputStream stream = null;
@@ -93,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         }else if (liza.isEnabled()){
             stream = getResources().openRawResource(R.raw.lizaspringschedule);
         }else{
+
             Toast toast = Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG);
             toast.show();
         }
@@ -101,16 +94,29 @@ public class MainActivity extends AppCompatActivity {
         BufferedReader buffreader = new BufferedReader(inputreader);
         String line;
         StringBuilder text = new StringBuilder();
+        String htmlSch = null;
+
+
+
+       classList = new ArrayList<>();
+       String classHtmlSection = null;
+
+       Document doc = null;
 
         try {
             while((line = buffreader.readLine()) != null) {
-                text.append(line);
 
+                text.append(line);
+                classHtmlSection = text.toString();
+
+                //doc = Jsoup.parse(htmlSch);
+                //classHtmlSection = doc.text();
             }
+
         }catch (IOException e){
-            e.printStackTrace();
+            Log.d("Error", "someOtherMethod()");
         }
 
-        return text.toString();
+        return classHtmlSection;
     }
 }
