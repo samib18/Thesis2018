0c28a2e7ffb8c9ccd5719ab3145e054b82198c94
samib18 <sami.bialozynski@gmail.com>
2018-02-03 13:48:00 -0500

    Add Project

--- app/src/main/java/edu/samibialozynski2washcoll/thesis2018/MainActivity.java
+++ app/src/main/java/edu/samibialozynski2washcoll/thesis2018/MainActivity.java
@@ -4,34 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView schedule;
-    ArrayList<String> classList;
     Button sami;
     Button elle;
     Button court;
     Button liza;
-    ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         sami = findViewById(R.id.samiButton);
         elle = findViewById(R.id.elleButton);
@@ -44,23 +42,24 @@ public class MainActivity extends AppCompatActivity {
 
                 schedule = findViewById(R.id.samiSchedule);
                 schedule.setText(getSchedule());
-
             }
         });
 
         elle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 schedule = findViewById(R.id.samiSchedule);
-                //schedule.setAdapter(getSchedule());
+                schedule.setText(getSchedule());
             }
         });
 
         court.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 schedule = findViewById(R.id.samiSchedule);
-                //schedule.setAdapter(getSchedule());
+                schedule.setText(getSchedule());
             }
         });
 
@@ -69,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 schedule = findViewById(R.id.samiSchedule);
-                //schedule.setAdapter(getSchedule());
+                schedule.setText(getSchedule());
             }
         });
     }
@@ -94,29 +93,26 @@ public class MainActivity extends AppCompatActivity {
         BufferedReader buffreader = new BufferedReader(inputreader);
         String line;
         StringBuilder text = new StringBuilder();
-        String htmlSch = null;
 
-
-
-       classList = new ArrayList<>();
-       String classHtmlSection = null;
-
-       Document doc = null;
+        String classHtmlSection = null;
+        Elements classes = null;
+        Element places = null;
 
         try {
             while((line = buffreader.readLine()) != null) {
 
                 text.append(line);
                 classHtmlSection = text.toString();
-
-                //doc = Jsoup.parse(htmlSch);
-                //classHtmlSection = doc.text();
             }
+
+            Document doc = Jsoup.parse(classHtmlSection);
+            classes = doc.getElementsByClass("schedule-listitem-header-title");
+            places = doc.getElementById("text: ko.utils.unwrapObservable(MeetingLocation)");
 
         }catch (IOException e){
             Log.d("Error", "someOtherMethod()");
         }
 
-        return classHtmlSection;
+        return classes.text();
     }
 }
